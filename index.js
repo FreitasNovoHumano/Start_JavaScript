3457fa77f563f4b69e4d517515cf934b8b1e6f63
Fábio Freitas <ecommercefreitas@gmail.com>
2025-01-06 21:22:54 -0300

    Tipo de dados, Strings

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 alert("Començando meus estudos de JavaScript")
 
 console.log("Estou usando a liguagem Javascript")
+
+var texto = "Olá Mundo"
+
+var numero = 99
+
+var nome = "Fabio"
+let nome = "Lucas" // variavel pode ser alterada. Sobescreve o valor anterior
+
+const numero = 42 // variavel constante não pode ser alterada
+
+console.log(texto)
+console.log(texto)
+console.log(texto)
+console.log(texto)
+
+console.log(numero)
+
+console.log(nome)
