45eb0f64d762cbe035c9ba11338809aded2cc7d1
Fábio Freitas <ecommercefreitas@gmail.com>
2025-02-14 22:47:05 -0300

    Estrutura de repetição For

--- estruturaDeControle.js
+++ estruturaDeControle.js
@@ -24,12 +24,12 @@ switch ("Brasil"){
 }
 
 //ESTRUTURA DE REPETIÇÃO WHILE, DO WHILE, FOR
-let numero = 9
+let numero3 = 9
 
-while (numero > 1){
+while (numero3 > 1){
   alert("O número é " + numero)
   alert("Diminuindo o número...")
-  numero = numero - 1
+  numero3 = numero3 - 1
 }
 
 let opcao = prompt("Escolha uma opção: 1. teste\n2. teste 2\n3. sair")
@@ -45,3 +45,15 @@ do{
 } while (opcao !== "3")
 
 alert("Você wscolheu sair...")
+
+//ESTRUTURA DE REPETIÇÃO FOR
+let numero4 = parseFloat(prompt("Escolha um número"))
+alert("O número é " + numero)
+
+//parte 1: código executado antes da condição
+//parte 2: verificação da condição
+//parte 3: código executado após o bloco do for
+for (let i = 0; numero > i;) {
+  alert('${numero} é maior do que i - i = ${i}')
+  i = i + 1    
+  }
