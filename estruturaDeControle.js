479678d15a99f60db829fa0796865da13e94541d
Fábio Freitas <ecommercefreitas@gmail.com>
2025-02-14 22:25:23 -0300

    Estrutura de Repetição While, Do While

--- estruturaDeControle.js
+++ estruturaDeControle.js
@@ -24,3 +24,24 @@ switch ("Brasil"){
 }
 
 //ESTRUTURA DE REPETIÇÃO WHILE, DO WHILE, FOR
+let numero = 9
+
+while (numero > 1){
+  alert("O número é " + numero)
+  alert("Diminuindo o número...")
+  numero = numero - 1
+}
+
+let opcao = prompt("Escolha uma opção: 1. teste\n2. teste 2\n3. sair")
+
+while (opcao !== "3"){
+  alert("Você escolheu a opção " + opcao)
+  opcao = prompt("Escolha uma opção: \n1. teste\n2. teste 2\n3. sair")
+}
+
+do{
+  alert("Você escolheu a opção " + opcao)
+  opcao = prompt("Escolha uma opção: \n1. teste\n2. teste 2\n3. sair")
+} while (opcao !== "3")
+
+alert("Você wscolheu sair...")
